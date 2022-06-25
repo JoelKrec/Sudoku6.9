1880ec96dd0fd03ebfe80a94141999905b95a399
Joel Krec <jk@iniationware.com>
2022-06-25 17:51:45 +0200

    feat: header adjusts to screen width now

--- JoelsParts/control_screen.c
+++ JoelsParts/control_screen.c
@@ -1,48 +1,69 @@
 #include "control_screen.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <windows.h>
 
 void show_control_screen()
 {
-   print_heading("Controls");
+   print_heading("Control");
 
    print_controls();
 }
 
 void print_controls()
 {
-    int controls_array_length = 4;
+    int controls_arrays_length = 4;
 
-    char* controls_array[4] = {"WASD or arrow keys  -   Move in fields or blocks",
-    "space bar   -   Switch between blocks and fields",
-    "C   -   Shows controls screen",
-    "check   -   Checks your board for completion"};
+    char* controls_keys_array[4] = {"WASD or arrow keys",
+    "space bar",
+    "C",
+    "check"};
 
-    for(int i = 0; i < controls_array_length; i++)
+    char* controls_definition_array[4] = {"Move in fields or blocks",
+    "Switch between blocks and fields",
+    "Shows controls screen",
+    "Checks your board for completion"};
+
+    char* filler = "    <-->   ";
+
+    for(int i = 0; i < controls_arrays_length; i++)
     {
         printf("\n");
 
-        int padding = (120 - strlen(controls_array[i])) / 2;
+        int padding = 60 - strlen(controls_keys_array[i]) - 7;
 
         for(int j = 0; j < padding; j++)
         {
             printf(" ");
         }
 
-        printf("%s\n", controls_array[i]);
+        printf("%s%s%s\n", controls_keys_array[i], filler, controls_definition_array[i]);
     }
 }
 
 void print_heading(char* header)
 {
-    print_full_line();
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int ret;
+    ret = GetConsoleScreenBufferInfo(GetStdHandle( STD_OUTPUT_HANDLE ),&csbi);
 
-    print_border_line();
+    int screen_width = csbi.dwSize.X;
+
+    print_full_line(screen_width);
+
+    print_border_line(screen_width);
 
     printf("##");
 
     int header_length = strlen(header);
 
-    int empty_space_next_to_header = (120 - 4 - header_length) / 2;
+    if(header_length % 2 != 0)
+    {
+        printf(" ");
+    }
+
+    int empty_space_next_to_header = (screen_width - 4 - header_length) / 2;
 
     for(int i = 0; i < empty_space_next_to_header; i++)
     {
@@ -60,23 +81,23 @@ void print_heading(char* header)
 
     printf("##");
 
-    print_border_line();
+    print_border_line(screen_width);
 
-    print_full_line();
+    print_full_line(screen_width);
 }
 
-void print_full_line()
+void print_full_line(int screen_width)
 {
-    for(int i = 0; i < 120; i++)
+    for(int i = 0; i < screen_width; i++)
     {
         printf("#");
     }
 }
 
-void print_border_line()
+void print_border_line(int screen_width)
 {
     printf("##");
-    for(int i = 0; i < 116; i++)
+    for(int i = 0; i < screen_width - 4; i++)
     {
         printf(" ");
     }
